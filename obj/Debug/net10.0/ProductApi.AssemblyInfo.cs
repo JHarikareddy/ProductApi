45f34e6281380844039a8484688ccc0f6c93ef42
Harika Reddy <harika.reddy@nexergroup.com>
2026-03-22 20:20:04 +0530

    Final Product API with CRUD, filtering, sorting, validation

--- obj/Debug/net10.0/ProductApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/ProductApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fabd4722e1521bd55ced2018cbc88f198fcd9cff")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
